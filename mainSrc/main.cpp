c6fe33de4f8d2140494ba1afc80ea5bafbf63dfd
pattersonz <pattersonz.se@gmail.com>
2019-03-06 21:33:06 -0600

    Integrated GLFW, have basic window running, calls vulkan commands

--- mainSrc/main.cpp
+++ mainSrc/main.cpp
@@ -1,13 +1,37 @@
-#define MAX_SOURCE_SIZE (0x100000)
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <vector>
-
 #include "Pipe.h"
-using std::vector;
+#include <iostream>
 
-void initializeOpenCL();
+using std::vector;
 
 int main ()
 {
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	std::cout << extensionCount << " extensions supported" << std::endl;
+
+	while (!glfwWindowShouldClose(window)) 
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
+	return 0;
+
+	
+
 	return 0;
 }
